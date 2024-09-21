693babdb4fd0f5483422dad354c7c70cb8f4bf46
Axel Velasquez <yoloax11@gmail.com>
2024-09-20 19:51:32 -0700

    updated to include the messagehistory from a request

--- backend/controllers/openAiController.js
+++ backend/controllers/openAiController.js
@@ -2,12 +2,14 @@ const openai = require("../config/openaiConfig");
 
 const generateMeta = async (req, res) => {
   const financialAdvise = req.body.message;
+  const messageHistory = req.body.messages;
   const description = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [
+      ...messageHistory, // Spreads the previous messages
       {
         role: "user",
-        content: `You are a professional financial advisor, help with ${financialAdvise} be concise, precise and brief on your answer, dont mention that you are a professional financial advisor`,
+        content: `You are a professional financial advisor, help with ${financialAdvise}. Be concise, precise, and brief in your answer. Do not mention that you are a professional financial advisor.`,
       },
     ],
     max_tokens: 100,
@@ -16,7 +18,6 @@ const generateMeta = async (req, res) => {
   res.status(200).json({
     aibot: description.choices[0].message.content,
   });
-
 };
 
 module.exports = { generateMeta };
