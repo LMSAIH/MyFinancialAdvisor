7de5aca0bbd09c11581a89dec997d924622a4340
Axel Velasquez <yoloax11@gmail.com>
2024-09-25 22:09:29 -0700

    updated the config

--- backend/config/openaiConfig.js
+++ backend/config/openaiConfig.js
@@ -1,9 +1,9 @@
 const { OpenAI } = require('openai');
 require('dotenv').config();
 
+//Take care of the client process to access the key
 const client = new OpenAI({
     apiKey: process.env.OPEN_AI_KEY, // This is the default and can be omitted
   });
-
 
 module.exports = client;
