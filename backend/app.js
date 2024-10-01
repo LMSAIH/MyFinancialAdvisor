0c2cde5faaeb82830b3928ff0c576b2cd473854d
Axel Velasquez <yoloax11@gmail.com>
2024-09-30 21:12:10 -0700

    changed the server

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,9 @@ const { generateMeta } = require('./controllers/openAiController');
 const app = express();
 
 
-//middleware
+//middleware to get the json
 app.use(express.json());
+//enabling cors because otherwise it will cause a bug
 app.use(cors());
 
 app.listen(3000, () => {console.log('Cors enabled web server listening to port 3000')});
