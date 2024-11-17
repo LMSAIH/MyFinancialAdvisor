6fa05db57d509d7cf7518552be0a6a7c290eac1b
Axel Velasquez <yoloax11@gmail.com>
2024-11-16 20:38:00 -0800

    updated the user context

--- frontend/src/hooks/useContextProvider.js
+++ frontend/src/hooks/useContextProvider.js
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { MessagesContext } from "../context/messagesContext";
 
+//provide the context for the user
 export const useMessagesProvider = () => {
 
     const context = useContext(MessagesContext);
